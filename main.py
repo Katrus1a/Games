4a64dba561f31c5ab61d02dc67b69723999722d8
bilKamaf <111289879+Katrus1a@users.noreply.github.com>
2024-08-20 23:58:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pygame
+import random
+import time
+
 size = [440, 600]
 
 Block_size = 20
@@ -28,3 +31,18 @@ while True:
                                                   20+row*Block_size +margin*(row+1), Block_size, Block_size])
 
     pygame.display.flip()
+
+clock = pygame.time.Clock()
+
+snake_size=20
+snake_speed=15
+message_font=pygame.font.SysFont('urbdvjbv', 42)
+score_font=pygame.font.SysFont('gnxxxxxxxxxxxxx', 42)
+
+def print_score(score):
+    text=score_font.render("Score:"+str(score), True, White_color)
+    game_display.blit(text,[0,0])
+
+def draw_snake (snake_size, snake_pixels):
+    for pixel in snake_pixels:
+        
