be62b06940f4565c00f2d65efb7fa10d39a82753
bilKamaf <111289879+Katrus1a@users.noreply.github.com>
2024-08-22 08:27:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -65,6 +65,24 @@ def run_game():
 
     while not game_over:
 
+        while game_close:
+            game.display.fill(black)
+            game_over_message=message_font.render("Game Over!", True, red)
+            game_display.blit(game_over_message, [width/3, height/3])
+            print_score(snake_length-1)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type==pygame.KEYDOWN:
+                    if event.key==pygame.K_1:
+                        game_over=True
+                        game_close=False
+                    if event.key==pygame.K_2:
+                        run_game()
+                    if event.type==pygame.QUIT:
+                        game_over = True
+                        game_close = False
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
@@ -82,11 +100,44 @@ def run_game():
                         x_speed == 0
                         y_speed = snake_size
 
-        # if x>=width or x<0 or y>=height or y<0:
-        #     game_close=True
-        #
-        # x+=x_speed
-        # y+=y_speed
+        if x>=width or x<0 or y>=height or y<0:
+            game_close=True
+
+        x+=x_speed
+        y+=y_speed
+
+        game.display.fill(black)
+        pygame.draw.rect(game_display, Violet_color, [target_x, target_y, snake_size, snake_size])
+
+        snake_pixels.append([x,y])
+
+        if len(snake_pixels)>snake_length:
+            del snake_pixels[0]
+
+            for pixel in snake_pixels[:-1]:
+                if pixel == [x, y]:
+                    game_close=True
+
+            draw_snake((snake_size, snake_pixels))
+            print_score(snake_length - 1)
+
+            pygame.display.update()
+
+            if x==target_x and y==target_y:
+                target_x = round(random.randrange(0, width - snake_size) / 10.0) * 10.0
+                target_y = round(random.randrange(0, height - snake_size) / 10.0) * 10.0
+                snake_length+=1
+
+
+            clock.tick(snake_speed)
+
+        pygame.quit()
+        quit()
+
+    run_game()
+
+
+
 
 
 
