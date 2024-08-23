31210a0e204996c7f214e40a1e28a96cc71a72df
bilKamaf <111289879+Katrus1a@users.noreply.github.com>
2024-08-23 23:48:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import random
 
 pygame.init()
 
-size=[400, 460]  
-info_panel_height=60  
+size=[400, 460]
+info_panel_height=60
 Block_size=20
 Block_count=20
 Frame_color=(16, 74, 53)
@@ -13,33 +13,75 @@ Blue_color=(10, 163, 112)
 Violet_color=(116, 38, 240)
 Black_color=(0, 0, 0)
 Red_color=(255, 0, 0)
-Gray_color = (105, 105, 105)
+Gray_color=(105, 105, 105)
 margin=1
 
 screen=pygame.display.set_mode(size)
 pygame.display.set_caption('SNAKE')
-
 
 clock=pygame.time.Clock()
 
 snake_size=20
 snake_speed=10
 
-message_font=pygame.font.SysFont('Arial', 42)
-score_font=pygame.font.SysFont('Arial', 30)
+message_font=pygame.font.SysFont('Arial', 36)
+score_font=pygame.font.SysFont('Arial', 28)
 
 start_time=pygame.time.get_ticks()
 
 food_blink=True
 
-def print_score(score):
-    text=score_font.render("Score:"+str(score), True, White_color)
-    screen.blit(text, [10, 10])
+def input_name_screen():
+    input_box=pygame.Rect(size[0]//4, size[1]//2-30, size[0]//2, 50)
+    color_inactive=White_color
+    color_active=Blue_color
+    color=color_inactive
+    active=False
+    text=''
+    done=False
 
-def print_time():
-    elapsed_time=(pygame.time.get_ticks()-start_time) // 1000 
-    time_text=score_font.render(f"Time: {elapsed_time} sec", True, White_color)
-    screen.blit(time_text, [size[0]-200, 10])
+    while not done:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                if input_box.collidepoint(event.pos):
+                    active=not active
+                else:
+                    active=False
+                color=color_active if active else color_inactive
+            if event.type==pygame.KEYDOWN:
+                if active:
+                    if event.key==pygame.K_RETURN:
+                        done=True
+                        return text
+                    elif event.key==pygame.K_BACKSPACE:
+                        text=text[:-1]
+                    else:
+                        text +=event.unicode
+
+        screen.fill(Frame_color)
+        txt_surface=score_font.render(text, True, color)
+        screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
+        pygame.draw.rect(screen, color, input_box, 2)
+
+        message=message_font.render("Enter Your Name", True, White_color)
+        start_message=score_font.render("Press Enter to Start", True, White_color)
+        screen.blit(message, [size[0]//4.5, size[1]//3.2])
+        screen.blit(start_message, [size[0]//4, size[1]//2+40])
+        pygame.display.flip()
+        clock.tick(30)
+
+def draw_grid():
+    for row in range(Block_count):
+        for column in range(Block_count):
+            if (row+column) % 2==0:
+                color=Blue_color
+            else:
+                color=White_color
+            pygame.draw.rect(screen, color,
+                             [column*Block_size, row*Block_size+info_panel_height, Block_size, Block_size])
 
 def draw_snake(snake_size, snake_pixels):
     length=len(snake_pixels)
@@ -50,24 +92,33 @@ def draw_snake(snake_size, snake_pixels):
             Violet_color[2]+(White_color[2]-Violet_color[2])*i//length
         )
         shadow_offset=3
-        pygame.draw.rect(screen, Gray_color, [pixel[0]+shadow_offset, pixel[1]+info_panel_height+shadow_offset, snake_size, snake_size])
+        pygame.draw.rect(screen, Gray_color,
+                         [pixel[0]+shadow_offset, pixel[1]+info_panel_height+shadow_offset, snake_size,
+                          snake_size])
         pygame.draw.rect(screen, Black_color, [pixel[0], pixel[1]+info_panel_height, snake_size, snake_size])
-        pygame.draw.rect(screen, gradient_color, [pixel[0]+2, pixel[1]+info_panel_height+2, snake_size-4, snake_size-4])
-def draw_grid():
-    for row in range(Block_count):
-        for column in range(Block_count):
-            if (row+column) % 2==0:
-                color=Blue_color
-            else:
-                color=White_color
-            pygame.draw.rect(screen, color, [column*Block_size, row*Block_size+info_panel_height, Block_size, Block_size])
+        pygame.draw.rect(screen, gradient_color,
+                         [pixel[0]+2, pixel[1]+info_panel_height+2, snake_size-4, snake_size-4])
 
-def game_over_screen():
-    screen.fill(Black_color)
-    game_over_message=message_font.render("Game Over!", True, Red_color)
+def print_score(score):
+    text=score_font.render("Score: "+str(score), True, White_color)
+    screen.blit(text, [10, 10])
+
+def print_time():
+    elapsed_time=(pygame.time.get_ticks()-start_time)//1000
+    time_text=score_font.render(f"Time: {elapsed_time} sec", True, White_color)
+    screen.blit(time_text, [size[0]-200, 10])
+
+def game_over_screen(player_name, score):
+    screen.fill(Frame_color)
+    game_over_message=message_font.render("Game Over!", True, White_color)
+    score_message=score_font.render(f"Score: {score}", True, White_color)
+    player_message=score_font.render(f"Player: {player_name}", True, White_color)
     try_again_message=score_font.render("Press R to Try Again", True, White_color)
-    screen.blit(game_over_message, [size[0]/3, size[1]/3])
-    screen.blit(try_again_message, [size[0]/3, size[1]/2])
+
+    screen.blit(game_over_message, [(size[0]-game_over_message.get_width())//2, size[1]//4])
+    screen.blit(score_message, [(size[0]-score_message.get_width())//2, size[1]//4+50])
+    screen.blit(player_message, [(size[0]-player_message.get_width())//2, size[1]//4+100])
+    screen.blit(try_again_message, [(size[0]-try_again_message.get_width())//2, size[1]//4+150])
     pygame.display.update()
 
     while True:
@@ -77,21 +128,21 @@ def game_over_screen():
                 quit()
             if event.type==pygame.KEYDOWN:
                 if event.key==pygame.K_r:
-                    run_game()
+                    run_game(player_name)
                 if event.key==pygame.K_q:
                     pygame.quit()
                     quit()
 
-def run_game():
-    global snake_speed  
+def run_game(player_name):
+    global snake_speed
     global start_time
     global food_blink
-    start_time=pygame.time.get_ticks()  
+    start_time=pygame.time.get_ticks()
     game_over=False
     game_close=False
 
     x=size[0]/2
-    y=(size[1]-info_panel_height)/2 
+    y=(size[1]-info_panel_height)/2
 
     x_speed=0
     y_speed=0
@@ -105,7 +156,7 @@ def run_game():
     while not game_over:
 
         while game_close:
-            game_over_screen()
+            game_over_screen(player_name, snake_length-1)
             break
 
         for event in pygame.event.get():
@@ -124,17 +175,17 @@ def run_game():
                 if event.key==pygame.K_DOWN:
                     x_speed=0
                     y_speed=snake_size
-               
-                if event.key==pygame.K_PLUS: #редагування швидкості
-                    snake_speed+=5
+
+                if event.key==pygame.K_PLUS:
+                    snake_speed +=5
                 if event.key==pygame.K_MINUS:
-                    snake_speed-=5
+                    snake_speed -= 5
 
         if x>=size[0] or x<0 or y>=size[1]-info_panel_height or y<info_panel_height:
             game_close=True
 
-        x+=x_speed
-        y+=y_speed
+        x +=x_speed
+        y +=y_speed
 
         screen.fill(Frame_color)
         draw_grid()
@@ -144,7 +195,7 @@ def run_game():
         else:
             food_color=White_color
 
-        pygame.draw.rect(screen, food_color, [target_x, target_y + info_panel_height, snake_size, snake_size])
+        pygame.draw.rect(screen, food_color, [target_x, target_y+info_panel_height, snake_size, snake_size])
         food_blink=not food_blink
 
         snake_pixels.append([x, y])
@@ -172,4 +223,5 @@ def run_game():
     pygame.quit()
     quit()
 
-run_game()
+player_name=input_name_screen()
+run_game(player_name)
